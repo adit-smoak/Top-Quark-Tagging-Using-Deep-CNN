6a54e349d2bca31f8e050d9c540b0fb52730db7d
adit-smoak <125796015+adit-smoak@users.noreply.github.com>
2025-07-18 21:06:31 +0530

    Update evaluate.m

--- model/evaluate.m
+++ model/evaluate.m
@@ -1,5 +1,5 @@
 load('testDataNew.mat');
-load('trainedModel90k.mat');      
+load('trainedModel.mat');  
 
 test_labels = categorical(test_labels);
 
@@ -15,3 +15,16 @@ disp("Test Accuracy: " + accuracy);
 figure;
 confusionchart(test_labels, YPred);
 title('Confusion Matrix on Test Set');
+
+YPredScores = predict(net, dsTest); 
+scorePosClass = YPredScores(:, 1);
+labelsLogical = (double(test_labels) == 1);
+
+[fpRate, tpRate, T, AUC] = perfcurve(labelsLogical, scorePosClass, true);
+
+figure;
+plot(fpRate, tpRate, 'b-', 'LineWidth', 2);
+xlabel('False Positive Rate');
+ylabel('True Positive Rate');
+title(['ROC Curve (AUC = ' num2str(AUC, '%.3f') ')']);
+grid on;
