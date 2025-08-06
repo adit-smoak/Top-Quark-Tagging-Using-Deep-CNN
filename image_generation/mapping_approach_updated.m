9281c2b75d3642fd928a314d12caee96a0e7134e
adit-smoak <125796015+adit-smoak@users.noreply.github.com>
2025-08-06 16:24:19 +0530

    Create mapping_approach_updated.m

---
+++ image_generation/mapping_approach_updated.m
@@ -0,0 +1,280 @@
+function [input_image, input_features, input_labels] = mapping_approach(file_path)
+
+    T_jets = parquetread(file_path);
+    
+    T_jets = movevars(T_jets, 'is_signal_new', 'Before', 'E_0');
+    T_jets = removevars(T_jets, 'x__index_level_0__');
+    
+    T_jets = sortrows(T_jets, 'is_signal_new');
+    % G = groupsummary(T_jets,'is_signal_new');
+    
+    Jet = T_jets_to_3d_tensor(T_jets);
+    
+    size(T_jets);
+    
+    function y = T_jets_to_3d_tensor(x)
+        y = x{:, 2 : 4 * 35 + 1}';
+        y = reshape(y, 4, 35, []);
+        y = pagetranspose(y);
+    end
+    
+    size(Jet);
+    
+    pT = sqrt(Jet(:, 2, :).^ 2 + Jet(:, 3, :).^ 2);
+    eta = squeeze(asinh(Jet(:, 4, :) ./ pT));
+    size(eta);
+    phi = squeeze(atan2(Jet(:, 3, :), Jet(:, 2, :)));
+    size(phi);
+    
+    pT = squeeze(pT);
+    size(pT);
+    
+    eta = eta - eta(1, :);
+    phi = phi - phi(1, :);
+    
+    Nb = 37;
+    Xedges = linspace(-1.6, 1.6, Nb + 1);
+    Yedges = linspace(-1.6, 1.6, Nb + 1);
+    
+    pixel_maps = cell(1, size(Jet, 3));
+    
+    for n = 1: size(Jet, 3)
+        row_idx = discretize(eta(:, n), Xedges);
+        col_idx = discretize(phi(:, n), Yedges);
+        pixel_map = containers.Map('KeyType', 'char', 'ValueType', 'any');
+    
+        for i = 1: 35 
+            r = row_idx(i);
+            c = col_idx(i);
+            if ~isnan(r) && ~isnan(c)
+                key = sprintf('%d, %d', r, c);
+                if isKey(pixel_map, key)
+                    pixel_map(key) = [pixel_map(key), i];
+                else
+                    pixel_map(key) = i;
+                end
+            end
+        end
+        pixel_maps{n} = pixel_map;
+    end
+    
+    num_jets = length(pixel_maps);
+    
+    % pixel wise channels
+    particle_count_channel = zeros(Nb, Nb, num_jets);
+    pT_sum_channel = zeros(Nb, Nb, num_jets);
+    pZ_sum_channel = zeros(Nb, Nb, num_jets);
+    energy_sum_channel = zeros(Nb, Nb, num_jets);
+    energy_variance_channel = zeros(Nb, Nb, num_jets);
+    pT_variance_channel = zeros(Nb, Nb, num_jets);
+    energy_skewness_pixel_channel = zeros(Nb, Nb, num_jets);
+    pT_skewness_pixel_channel = zeros(Nb, Nb, num_jets);
+    energy_kurtosis_pixel_channel = zeros(Nb, Nb, num_jets);
+    pT_kurtosis_pixel_channel = zeros(Nb, Nb, num_jets);
+    avg_energy_channel = zeros(Nb, Nb, num_jets);
+    angular_momentum_sum_channel = zeros(Nb, Nb, num_jets);
+    
+    for n = 1: num_jets
+        map = pixel_maps{n};
+        keys = map.keys;
+    
+        for k = 1: length(keys)
+            key = keys{k};
+            inds = map(key);
+            parts = sscanf(key, '%d, %d');
+            r = parts(1);
+            c = parts(2);
+    
+            if r >= 1 && r <= Nb && c >= 1 && c <= Nb
+                particle_count_channel(r, c, n) = numel(inds);
+                pT_sum = 0;
+                pZ = 0;
+                E_sum = 0;
+                angular_momentum_sum = 0;
+                energy = zeros(1, 35);
+                pt = zeros(1, 35);
+                it = 0;
+                for i = 1: length(inds)
+                    ind = inds(i);
+                    pT_sum = pT_sum + pT(ind, n);
+                    pZ = pZ + abs(Jet(ind, 4, n));
+                    E_sum = E_sum + Jet(ind, 1, n);
+                    it = it + 1;
+                    energy(it) = Jet(ind, 1, n);
+                    pt(it) = pT(ind, n);
+    
+                    r_dist = sqrt(eta(ind, n) ^ 2 + phi(ind, n) ^ 2);
+                    mom_eq = sqrt(pT(ind, n) ^ 2 + Jet(ind, 4, n) ^ 2);
+                    angular_momentum_sum = angular_momentum_sum + (r_dist * mom_eq);
+                end
+                pT_sum_channel(r, c, n) = pT_sum;
+                pZ_sum_channel(r, c, n) = pZ;
+                energy_sum_channel(r, c, n) = E_sum;
+                angular_momentum_sum_channel(r, c, n) = angular_momentum_sum;
+    
+                avg_energy_channel(r, c, n) = E_sum / length(inds);
+    
+                if length(inds) > 1          
+                    energy_variance_channel(r, c, n) = var(energy);
+                    pT_variance_channel(r, c, n) = var(pt);
+                    energy_skewness_pixel_channel(r, c, n) = skewness(energy);
+                    energy_kurtosis_pixel_channel(r, c, n) = kurtosis(energy);
+                    pT_skewness_pixel_channel(r, c, n) = skewness(pt);
+                    pT_kurtosis_pixel_channel(r, c, n) = kurtosis(pt);
+                else
+                    energy_skewness_pixel_channel(r, c, n) = 0;
+                    energy_kurtosis_pixel_channel(r, c, n) = 0;
+                    pT_skewness_pixel_channel(r, c, n) = 0;
+                    pT_kurtosis_pixel_channel(r, c, n) = 0;
+                end
+            end
+        end
+    
+    end
+        
+    for n= 1: num_jets
+        particle_count_channel(:, :, n) = align_img(particle_count_channel(:, :, n), n, eta, phi, Xedges, Yedges);
+        pT_sum_channel(:, :, n) = align_img(pT_sum_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        pZ_sum_channel(:, :, n) = align_img(pZ_sum_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        energy_sum_channel(:, :, n) = align_img(energy_sum_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        energy_variance_channel(:, :, n) = align_img(energy_variance_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        pT_variance_channel(:, :, n) = align_img(pT_variance_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        energy_skewness_pixel_channel(:, :, n) = align_img(energy_skewness_pixel_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        pT_skewness_pixel_channel(:, :, n) = align_img(pT_skewness_pixel_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        energy_kurtosis_pixel_channel(:, :, n) = align_img(energy_kurtosis_pixel_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        pT_kurtosis_pixel_channel(:, :, n) = align_img(pT_kurtosis_pixel_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        avg_energy_channel(:, :, n) = align_img(avg_energy_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+        angular_momentum_sum_channel(:, :, n) = align_img(angular_momentum_sum_channel(:, :, n), n, eta, phi, Xedges, Yedges); 
+    
+        fprintf('n: %f\n', n);
+        % ener_sum_top = sum(energy_sum_channel(1:18, :, n), 'all');
+        % ener_sum_bot = sum(energy_sum_channel(19:37, :, n), 'all');
+        % 
+        % if ener_sum_bot < ener_sum_top
+        %     flip_channels = {
+        %         'particle_count_channel',
+        %         'pT_sum_channel',
+        %         'pZ_sum_channel',
+        %         'energy_sum_channel',
+        %         'energy_variance_channel',
+        %         'pT_variance_channel',
+        %         'energy_skewness_pixel_channel',
+        %         'pT_skewness_pixel_channel',
+        %         'energy_kurtosis_pixel_channel',
+        %         'pT_kurtosis_pixel_channel',
+        %         'avg_energy_channel',
+        %         'angular_momentum_sum_channel'
+        %     };
+        % 
+        %     for k = 1:length(flip_channels)
+        %         eval([flip_channels{k} ' = flip(' flip_channels{k} ', 1);']);
+        %     end
+        % end
+
+    end
+    
+    particle_count_channel = zscore(particle_count_channel,0,3);             
+    pT_sum_channel = zscore(pT_sum_channel,0,3);  
+    pZ_sum_channel = zscore(pZ_sum_channel, 0, 3);
+    energy_sum_channel = zscore(energy_sum_channel, 0, 3);
+    energy_variance_channel = zscore(energy_variance_channel, 0, 3);
+    pT_variance_channel = zscore(pT_variance_channel, 0, 3);
+    energy_skewness_pixel_channel = zscore(energy_skewness_pixel_channel, 0, 3);
+    pT_skewness_pixel_channel = zscore(pT_skewness_pixel_channel, 0, 3);
+    energy_kurtosis_pixel_channel = zscore(energy_kurtosis_pixel_channel, 0, 3);
+    pT_kurtosis_pixel_channel = zscore(pT_kurtosis_pixel_channel, 0, 3);
+    avg_energy_channel = zscore(avg_energy_channel, 0, 3);
+    angular_momentum_sum_channel = zscore(angular_momentum_sum_channel, 0, 3);
+
+    input_image = zeros(37, 37, 12, num_jets);
+    
+    input_image(:, :, 1, :) = particle_count_channel;
+    input_image(:, :, 2, :) = pT_sum_channel;
+    input_image(:, :, 3, :) = pZ_sum_channel;
+    input_image(:, :, 4, :) = energy_sum_channel;
+    input_image(:, :, 5, :) = energy_variance_channel;
+    input_image(:, :, 6, :) = pT_variance_channel;
+    input_image(:, :, 7, :) = energy_skewness_pixel_channel;
+    input_image(:, :, 8, :) = pT_skewness_pixel_channel;
+    input_image(:, :, 9, :) = energy_kurtosis_pixel_channel;
+    input_image(:, :, 10, :) = pT_kurtosis_pixel_channel;
+    input_image(:, :, 11, :) = avg_energy_channel;
+    input_image(:, :, 12, :) = angular_momentum_sum_channel;
+    
+    input_labels = T_jets.is_signal_new;
+
+    % global image features
+    input_features = zeros(1, 1, 4, num_jets);
+
+    for n = 1: num_jets
+        energy_radial = calculate_radial_profile(energy_sum_channel(:, :, n));
+        input_features(1, 1, 1, n) = skewness(energy_radial);
+        input_features(1, 1, 2, n) = kurtosis(energy_radial);
+    
+        pT_radial = calculate_radial_profile(pT_sum_channel(:, :, n));
+        input_features(1, 1, 3, n) = skewness(pT_radial);
+        input_features(1, 1, 4, n) = kurtosis(pT_radial);
+    end
+
+    data_norm = input_features; 
+    
+    for feature = 1:4
+        feature_values = squeeze(input_features(1,1,feature,:)); 
+        mu = mean(feature_values);
+        sigma = std(feature_values);
+    
+        normalized = (feature_values - mu) / sigma;
+    
+        data_norm(1,1,feature,:) = reshape(normalized, [1 1 1 num_jets]);
+    end
+
+    input_features = data_norm;
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function radial_array = calculate_radial_profile(image)
+    radial_array = zeros(1, 19);
+    center_row = 19;
+    center_col = 19;
+    
+    radial_array(1) = image(center_row, center_col);
+    
+    for ring = 1:18
+        ring_sum = 0;
+        
+        for row = 1:37
+            for col = 1:37
+                distance = max(abs(row - center_row), abs(col - center_col));
+                if distance == ring
+                    ring_sum = ring_sum + image(row, col);
+                end
+            end
+        end
+        
+        radial_array(ring + 1) = ring_sum;
+    end
+end
